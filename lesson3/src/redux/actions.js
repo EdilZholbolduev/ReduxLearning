0c45088ee3d03d8c7d5127284b33f6375258924e
Edil Zholbolduev <tashtan841@gmail.com>
2023-11-22 10:56:19 +0200

    working with fetch request and working with json format data

--- lesson3/src/redux/actions.js
+++ lesson3/src/redux/actions.js
@@ -23,3 +23,10 @@ export function getUsersAction(users){
 
 
 
+export function fetchUsersAction(){
+   return async function (dispatch){
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      dispatch (getUsersAction(data))
+   }
+}
