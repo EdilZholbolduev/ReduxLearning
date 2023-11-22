fec2bf5f9e5de46ce398f89f97ae16cd50d07ec0
Edil Zholbolduev <tashtan841@gmail.com>
2023-11-22 11:10:12 +0200

    getting users data by id

--- lesson3/src/components/user.js
+++ lesson3/src/components/user.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { fetchUserOneInfo } from "../redux/actions";
 
 
 function User({userInfo}){
+
+   const dispatch = useDispatch()
+
+   const getOneUser = (event)=>{
+      dispatch(fetchUserOneInfo(event.target.value))
+   }
+
    return(
       <ul>
          <li>user: {userInfo.name}</li>
          <li>email: {userInfo.email}</li>
 
 
-         <button>get mode</button>
+         <button value={userInfo.id} onClick={getOneUser}>get mode</button>
       </ul>
    )
 }
--- lesson3/src/redux/actions.js
+++ lesson3/src/redux/actions.js
@@ -30,3 +30,12 @@ export function fetchUsersAction(){
       dispatch (getUsersAction(data))
    }
 }
+
+
+export function fetchUserOneInfo(id){
+   return async function (){
+      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      const data = await response.json()
+      console.log(data)
+   }
+}
