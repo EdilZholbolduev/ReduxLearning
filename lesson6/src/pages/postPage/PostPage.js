b03f00faca56d97577d997779196f869f46084a4
Edil Zholbolduev <tashtan841@gmail.com>
2023-12-05 16:42:53 +0200

    addign more function

--- lesson6/src/pages/postPage/PostPage.js
+++ lesson6/src/pages/postPage/PostPage.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getPosts } from "../../store/PostsSlice";
+import { getPosts, postsDelete } from "../../store/PostsSlice";
 import Post from "../../components/Post";
+import PostPreloader from "../../components/PostPreloader";
 
 function PostPage(){
 
-   const {posts} = useSelector(state => state.postsReducer)
+   const {posts, preloader, message} = useSelector(state => state.postsReducer)
    const dispatch = useDispatch()
 
    const getPostsFunc = () =>{
       dispatch(getPosts())
    }
 
-   console.log(posts)
+   useEffect(()=>{
+      getPostsFunc()
+   }, [])
 
    return(
       <div>
          <button onClick={getPostsFunc}>get posts</button>
-         
-         {posts.map(post => <Post key={post.id} postInfo={post}/>)}
+         <button onClick={()=> dispatch(postsDelete())}>delete all</button>
+
+         {preloader ? <PostPreloader/> 
+         : message ? 
+               <div>
+                  <h2>{message}</h2>
+                  {posts.map(post => <Post key={post.id} postInfo={post}/>)}
+               </div> : <></>
+         }
       </div>
    )
 }
--- lesson6/src/store/PostsSlice.js
+++ lesson6/src/store/PostsSlice.js
@@ -3,26 +3,52 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const getPosts = createAsyncThunk(
    'getPosts',
    async function (info, {dispatch, rejectWithValue}){
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const posts = await response.json()
-      dispatch(postsInfo(posts))
+      try{
 
+         dispatch(preloaderOn())
+         const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+         const posts = await response.json()
+         dispatch(postsInfo(posts))
+         if(response.status >= 200 && response.status <= 204 ){
+            dispatch(setMessage('status successfully'))
+         }else if(response.status === 404){
+            throw '404 not found'
+         }
+      }catch (e){
+         dispatch(setMessage(e))
+      }finally{
+         dispatch(preloaderOff())
+      }
    }
 )
 
 const postsSlice = createSlice({
    name: 'postsSlice',
    initialState: {
-      posts:[]
+      posts:[],
+      preloader: false,
+      message: ''
    },
    reducers: {
       postsInfo: (state, action)=>{
          state.posts = action.payload
+      },
+      postsDelete: (state, action)=>{
+         state.posts = []
+      },
+      preloaderOn: (state, action)=>{
+         state.preloader = true
+      },
+      preloaderOff: (state, action)=>{
+         state.preloader = false
+      },
+      setMessage: (state, action) =>{
+         state.message = action.payload
       }
    }
 })
 
 
-export const {postsInfo} = postsSlice.actions
+export const {postsInfo, postsDelete, preloaderOn, preloaderOff, setMessage } = postsSlice.actions
 
 export default postsSlice.reducer
